cb2cc434ffd88f259fa47c14fed6e2ac0d96bd8f
czke33 <czke33@gmail.com>
2024-04-08 08:53:04 +0300

    index needed to feedback option component

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,24 +1,27 @@
 import PropTypes from 'prop-types';
 import style from "./feedbackoptions.module.css"
-import React from'react';
+import React, {Fragment} from'react';
 
-function FeedbackOptions({ options, onLeaveFeedback, state }) {
-    const optionKeys = Object.keys(options)
+function FeedbackOptions({ options, onLeaveFeedback}) {
+    const optionKeys = Object.keys(options);
     
-    return(<>
-            <div className={style.feedbackcontainer}>
-            {optionKeys.map((option, index) => {
+    return (<>
+           <div className={style.feedbackcontainer}>
+            {optionKeys.map((option,index) => {
+                
                     return (
                         <>
-                     <li className={style.feedbackoptions} key={index}>
+                            <Fragment key={index}>
+                     <li className={style.feedbackoptions} >
                     <button
-                        type="button"
+                                    type="button"
+                                    key={index}
                                     name={option}
                                     value={option}
                         onClick={onLeaveFeedback}
                         className={style.feedbackbtn}
                     >{option}
-                    </button></li></>)
+                    </button></li></Fragment></>)
                 })}
         </div>
         </>)
@@ -29,6 +32,7 @@ function FeedbackOptions({ options, onLeaveFeedback, state }) {
 FeedbackOptions.propTypes = {
     options: PropTypes.object.isRequired,
     onLeaveFeedback: PropTypes.func.isRequired,
+    index: PropTypes.string.isRequired,
 }
 
 export default FeedbackOptions;
